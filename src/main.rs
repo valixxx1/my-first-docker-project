1c95bef5e554f6cfc5c5cc8a07729b1484bb6428
vgershun <vgershun@gmail.com>
2024-09-21 17:24:31 +0300

    hello world http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = std::net::TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        let string = std::fs::read_to_string("index.html").unwrap();
+        let content = format!("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n{}", string.len(), string);
+        stream.write_all(content.as_bytes()).unwrap();
+    }
 }
